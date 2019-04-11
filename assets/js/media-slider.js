c7b9566c74ebef1059b843be0dc144a4f4d8cd01
Brett Doyle <brettd3000@gmail.com>
2019-04-11 13:10:15 +0100

    Added media slider & video enhancement scripts

---
+++ assets/js/media-slider.js
@@ -0,0 +1,137 @@
+;(function() {
+    var vendors = ['ms', 'moz', 'webkit', 'o']
+
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame =
+            window[vendors[x] + 'RequestAnimationFrame']
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback) {
+            window.setTimeout(callback, 100)
+        }
+})()
+
+var all_images = document.querySelectorAll('.media.images.slider.slider')
+
+function setupImage(element) {
+    var sliderElement = document.createElement('div')
+    sliderElement.className = 'slider'
+
+    sliderElement.appendChild(document.createElement('div'))
+    sliderElement.lastElementChild.className = 'line'
+    sliderElement.appendChild(document.createElement('div'))
+    sliderElement.lastElementChild.className = 'button'
+
+    element.lastElementChild.lastElementChild.insertBefore(
+        sliderElement,
+        element.lastElementChild.lastElementChild.firstChild
+    )
+
+    var inputs = element.querySelectorAll('input')
+
+    var options = inputs.length
+    var activeOption = 0
+    var activePosition = 0
+
+    var mouse_down = false
+    var updateRequested = false
+
+    var sliderRect = {
+        left: 0,
+        width: 0
+    }
+
+    function updatePosition() {
+        updateRequested = false
+
+        sliderElement.lastElementChild.style.left =
+            activePosition.toString() + '%'
+    }
+
+    function updateSliderSize() {
+        sliderRect = sliderElement.getBoundingClientRect()
+    }
+
+    function onSliderDrag(event) {
+        if (event.offsetX === 0) {
+            return false
+        }
+
+        var position = Math.max(
+            0,
+            Math.min(1, (event.pageX - sliderRect.left) / sliderRect.width)
+        )
+
+        activeOption = Math.floor(position * options)
+
+        activePosition = position * 100
+
+        inputs[activeOption].checked = true
+
+        if (!updateRequested) {
+            updateRequested = true
+            requestAnimationFrame(updatePosition)
+        }
+    }
+
+    function onInputSelect() {
+        var old = activeOption
+        activeOption = this._index
+
+        activePosition = (100 / options) * activeOption + 50 / options
+
+        sliderElement.lastElementChild.style.transition =
+            'left ' + (0.1 * Math.abs(old - activeOption)).toString() + 's'
+
+        if (!updateRequested) {
+            updateRequested = true
+            requestAnimationFrame(updatePosition)
+        }
+    }
+
+    requestAnimationFrame(updateSliderSize)
+    window.addEventListener('resize', updateSliderSize)
+
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i]._index = i
+        inputs[i].addEventListener('change', onInputSelect)
+
+        if (inputs[i].checked) {
+            onInputSelect.call(inputs[i])
+        }
+    }
+
+    sliderElement.addEventListener('mousedown', function(event) {
+        mouse_down = true
+
+        onSliderDrag(event)
+
+        sliderElement.lastElementChild.style.transition = ''
+    })
+
+    sliderElement.addEventListener('mousemove', function(event) {
+        if (mouse_down !== false) {
+            onSliderDrag(event)
+        }
+    })
+
+    window.addEventListener('mouseup', function() {
+        if (mouse_down) {
+            mouse_down = false
+
+            activePosition = (100 / options) * activeOption + 50 / options
+
+            sliderElement.lastElementChild.style.transition = 'left 0.1s'
+
+            if (!updateRequested) {
+                updateRequested = true
+                requestAnimationFrame(updatePosition)
+            }
+        }
+    })
+}
+
+for (var i = 0; i < all_images.length; i++) {
+    setupImage(all_images[i])
+}
--- assets/js/media-video.js
+++ assets/js/media-video.js
@@ -1,43 +1,158 @@
-window.onload = function() {
-    var all_videos = document.querySelectorAll('.media.video')
+;(function() {
+    var vendors = ['ms', 'moz', 'webkit', 'o']
+
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame =
+            window[vendors[x] + 'RequestAnimationFrame']
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback) {
+            window.setTimeout(callback, 100)
+        }
+})()
+
+var all_videos = document.querySelectorAll('.media.video')
+
+function setupVideo(element) {
+    var video = element.querySelector('video')
+
+    element.lastElementChild.appendChild(document.createElement('div'))
+    element.lastElementChild.lastElementChild.className = 'selectors'
+
+    var playButton = document.createElement('label')
+    playButton.appendChild(document.createElement('span'))
+    playButton.lastElementChild.textContent = '►'
+
+    element.lastElementChild.lastElementChild.appendChild(playButton)
+
+    playButton._video_element = video
+    playButton.addEventListener('click', onButtonClick)
+
+    var timeline = document.createElement('div')
+    timeline.className = 'slider'
+
+    timeline.appendChild(document.createElement('div'))
+    timeline.lastElementChild.className = 'button'
+    timeline.appendChild(document.createElement('div'))
+    timeline.lastElementChild.className = 'line'
+
+    timeline._video_element = video
+
+    element.lastElementChild.lastElementChild.appendChild(timeline)
+
+    var playing = false
+    var seeking_paused = false
+
+    var updateRequested = false
+    var mouse_down = false
+
+    var sliderRect = {
+        left: 0,
+        width: 0
+    }
 
     function onButtonClick() {
-        if (this._video_element.paused) {
-            this._video_element.play()
+        playing = !playing
 
-            this.textContent = '❚❚'
-            this.className = 'active'
-        } else {
-            this._video_element.pause()
+        if (playing) {
+            video.play()
 
-            this.textContent = '►'
-            this.className = ''
-        }
-    }
-
-    function setupVideo(element) {
-        var video = element.querySelector('video')
-
-        element.lastChild.appendChild(document.createElement('div'))
-        element.lastChild.lastChild.className = 'selectors'
-
-        var playButton = document.createElement('label')
-        playButton.textContent = '►'
-
-        element.lastChild.lastChild.appendChild(playButton)
-
-        playButton._video_element = video
-        playButton.addEventListener('click', onButtonClick)
-
-        if (video.paused) {
-            onButtonClick.call(playButton)
-        } else {
-            playButton.textContent = '❚❚'
+            playButton.lastElementChild.textContent = '❚❚'
             playButton.className = 'active'
+
+            timeline.className = 'slider active'
+        } else {
+            video.pause()
+
+            playButton.lastElementChild.textContent = '►'
+            playButton.className = ''
+
+            timeline.className = 'slider'
         }
     }
 
-    for (var i = 0; i < all_videos.length; i++) {
-        setupVideo(all_videos[i])
+    function onVideoUpdate() {
+        timeline.firstElementChild.style.left =
+            ((video.currentTime / video.duration) * 100).toString() + '%'
+
+        if (!video.paused) {
+            requestAnimationFrame(onVideoUpdate)
+        } else {
+            updateRequested = false
+        }
     }
+
+    function updateSliderSize() {
+        sliderRect = timeline.getBoundingClientRect()
+    }
+
+    function onTimelineDrag(event) {
+        var position = (event.pageX - sliderRect.left) / sliderRect.width
+
+        video.currentTime = position * video.duration
+
+        if (!updateRequested) {
+            updateRequested = true
+            requestAnimationFrame(onVideoUpdate)
+        }
+    }
+
+    requestAnimationFrame(updateSliderSize)
+    window.addEventListener('resize', updateSliderSize)
+
+    if (video.paused) {
+        onButtonClick()
+    } else {
+        playing = true
+
+        playButton.lastElementChild.textContent = '❚❚'
+        playButton.className = 'active'
+
+        timeline.classList = 'slider active'
+
+        updateRequested = true
+        requestAnimationFrame(onVideoUpdate)
+    }
+
+    video.addEventListener('play', function() {
+        if (!updateRequested) {
+            updateRequested = true
+            requestAnimationFrame(onVideoUpdate)
+        }
+    })
+
+    timeline.addEventListener('mousedown', function(event) {
+        mouse_down = true
+
+        if (!video.paused) {
+            video.pause()
+        }
+
+        seeking_paused = true
+
+        onTimelineDrag(event)
+    })
+
+    timeline.addEventListener('mousemove', function(event) {
+        if (mouse_down !== false) {
+            onTimelineDrag(event)
+        }
+    })
+
+    window.addEventListener('mouseup', function() {
+        if (mouse_down) {
+            mouse_down = false
+
+            if (seeking_paused && playing) {
+                video.play()
+
+                seeking_paused = false
+            }
+        }
+    })
+}
+
+for (var i = 0; i < all_videos.length; i++) {
+    setupVideo(all_videos[i])
 }
